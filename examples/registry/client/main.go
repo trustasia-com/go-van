a354b4b9930be36c595b61117b6788f5a7d08059
chenqijing <chenqijing@medlinker.com>
2021-05-27 16:50:34 +0800

    chore: registry examples

---
+++ examples/registry/client/main.go
@@ -0,0 +1,47 @@
+// Package main provides ...
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/deepzz0/go-van/pkg/codes"
+	"github.com/deepzz0/go-van/pkg/codes/status"
+	"github.com/deepzz0/go-van/pkg/registry"
+	"github.com/deepzz0/go-van/pkg/registry/etcd"
+	"github.com/deepzz0/go-van/pkg/server"
+	"github.com/deepzz0/go-van/pkg/server/grpcx"
+)
+
+func main() {
+	reg := etcd.NewRegistry(
+		registry.WithTTL(time.Second*10),
+		registry.WithAddress("localhost:2379"),
+	)
+
+	// grpc client
+	conn, err := grpcx.DialContext(
+		server.WithEndpoint("discov:///grpc"),
+		server.WithRegistry(reg),
+	)
+	if err != nil {
+		panic(err)
+	}
+	cli := pb.NewGreeterClient(conn)
+
+	for _, name := range []string{"go-van", "error", "panic"} {
+		reply, err := cli.SayHello(context.Background(), &pb.HelloRequest{Name: name})
+		if err != nil {
+			code := status.Code(err)
+			if code == codes.InvalidArgument {
+				fmt.Println("codes.InvalidArgument: ", err.Error())
+			} else {
+				fmt.Println(err.Error())
+			}
+			continue
+		}
+		fmt.Printf("[gRPC] SayHello %+v\n", reply)
+	}
+
+}
