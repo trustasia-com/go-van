64007a78c0d72ed048d0ab379dc9e2b04cb0b5d1
henry.chen <henry.chen@trustasia.com>
2022-05-27 14:44:21 +0800

    chore(httpx): update request struct

--- pkg/server/httpx/request.go
+++ pkg/server/httpx/request.go
@@ -2,16 +2,18 @@
 package httpx
 
 import (
+	"bytes"
 	"context"
-	"io"
 	"net/http"
+	"net/url"
 )
 
 // NewRequest new request for client
-func NewRequest(method, path string, body io.Reader) *Request {
+func NewRequest(method, path, query string, body []byte) *Request {
 	req := &Request{
 		method: method,
 		path:   path,
+		query:  query,
 		body:   body,
 
 		header: make(map[string][]string),
@@ -21,18 +23,32 @@ func NewRequest(method, path string, body io.Reader) *Request {
 
 // Request for http request
 type Request struct {
-	method             string    // http method
-	path               string    // url path & query
-	body               io.Reader // request body
-	username, password string    // basic auth
+	method             string // http method
+	path               string // url path
+	query              string // query
+	body               []byte // request body
+	username, password string // basic auth
 
 	header  http.Header     // header
 	context context.Context // context
 }
 
 // HTTP generate http request
-func (req *Request) HTTP(host string) (*http.Request, error) {
-	httpReq, err := http.NewRequest(req.method, host+req.path, req.body)
+// host, eg. https://example.com:8080
+// path, eg. /users
+// query, eg. page=1&size=10
+func (req *Request) HTTP(host string) (httpReq *http.Request, err error) {
+	u, err := url.Parse(host)
+	if err != nil {
+		return nil, err
+	}
+	u.Path = req.path
+	u.RawQuery = req.query
+	if len(req.body) > 0 {
+		httpReq, err = http.NewRequest(req.method, u.String(), bytes.NewReader(req.body))
+	} else {
+		httpReq, err = http.NewRequest(req.method, u.String(), nil)
+	}
 	if err != nil {
 		return nil, err
 	}
@@ -40,7 +56,7 @@ func (req *Request) HTTP(host string) (*http.Request, error) {
 	if req.context != nil {
 		httpReq = httpReq.WithContext(req.context)
 	}
-	if req.header != nil {
+	if len(req.header) > 0 {
 		httpReq.Header = req.header
 	}
 	if req.username != "" && req.password != "" {
@@ -49,15 +65,40 @@ func (req *Request) HTTP(host string) (*http.Request, error) {
 	return httpReq, nil
 }
 
-// SetBasicAuth basic auth
-func (req *Request) SetBasicAuth(username, password string) {
-	req.username = username
-	req.password = password
+// GetMethod return method
+func (req *Request) GetMethod() string {
+	return req.method
+}
+
+// GetPath return path
+func (req *Request) GetPath() string {
+	return req.path
+}
+
+// GetQuery return query
+func (req *Request) GetQuery() string {
+	return req.query
+}
+
+// GetBody return body
+func (req *Request) GetBody() []byte {
+	return req.body
 }
 
 // GetHeader get header
 func (req *Request) GetHeader() http.Header {
 	return req.header
+}
+
+// GetContext return context
+func (req *Request) GetContext() context.Context {
+	return req.context
+}
+
+// SetBasicAuth basic auth
+func (req *Request) SetBasicAuth(username, password string) {
+	req.username = username
+	req.password = password
 }
 
 // SetHeader set http header
@@ -70,7 +111,7 @@ func (req *Request) AddHeader(key, val string) {
 	req.header.Add(key, val)
 }
 
-// Context set context
-func (req *Request) Context(ctx context.Context) {
+// SetContext set context
+func (req *Request) SetContext(ctx context.Context) {
 	req.context = ctx
 }
