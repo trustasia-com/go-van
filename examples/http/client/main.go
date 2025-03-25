868c91d385dbe50fc867a494d850523f23a24ab3
henry.chen <henry.chen@trustasia.com>
2025-03-25 14:52:19 +0800

    chore: update examples

--- examples/http/client/main.go
+++ examples/http/client/main.go
@@ -13,6 +13,10 @@ import (
 func main() {
 	cli := httpx.NewClient(
 		server.WithEndpoint("https://api.deepzz.com/box-api/v1"),
+		httpx.WithHeader(map[string]string{
+			"lang":         "zh-CN",
+			"Content-Type": "application/json",
+		}),
 	)
 
 	req := httpx.NewRequest(http.MethodGet, "/user/profile", "", nil)
