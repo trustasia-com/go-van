3dce2207c29b969af8ef34edb1e6ea0fdc178ee2
henry.chen <henry.chen@trustasia.com>
2025-04-09 17:49:41 +0800

    chore(logx): more test

--- pkg/logx/logx_test.go
+++ pkg/logx/logx_test.go
@@ -39,6 +39,25 @@ func TestEntry(t *testing.T) {
 	WithContext(context.Background()).Info("test")
 }
 
+func TestNewLogging(t *testing.T) {
+	logger := NewLogging(
+		WithLevel(LevelError),
+		WithService("test-service"),
+		WithFlag(FlagFile),
+	)
+	logger.Debug("test")
+	logger.Debugf("test: %s", "hello")
+
+	logger.Info("test")
+	logger.Infof("test: %s", "hello")
+
+	logger.Warning("test")
+	logger.Warningf("test: %s", "hello")
+
+	logger.Error("test")
+	logger.Errorf("test: %s", "hello")
+}
+
 func BenchmarkLogging(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		Info(fmt.Sprint(i))
